be51f48797ec31d05aed09edf1903883943921fb
LUAN Junqing <junqing.luan@foxmail.com>
2019-02-17 17:47:30 +0100

    modified

--- Filter_LUAN.cpp
+++ Filter_LUAN.cpp
@@ -2,7 +2,7 @@
  * @Author: LUAN Junqing
  * @Date: 2019-02-17 11:55:24
  * @Last Modified by: LUAN Junqing
- * @Last Modified time: 2019-02-17 13:44:43
+ * @Last Modified time: 2019-02-17 17:45:55
  */
 
 
@@ -24,7 +24,7 @@ void imgToGray(IplImage *Image_IN, uchar *Data_out, int step_gray){
 	Data_in = (uchar *)Image_IN -> imageData;
 	for(i = 0; i < height; ++i){
 		for(j = 0; j < width; ++j){
-			Data_out[i*step_gray+j] = 0.114*Data_in[i*step+j*channels+0] + 0.587*Data_in[i*step+j*channels+1] + 0.299*Data_in[i*step+j*channels+2];
+			Data_out[i*step_gray + j] = 0.114*Data_in[i*step + j*channels + 0] + 0.587*Data_in[i*step + j*channels + 1] + 0.299*Data_in[i*step + j*channels + 2];
 		}
 	}
 }
@@ -35,30 +35,29 @@ int compare(const void *f1, const void *f2){
 
 unsigned int sum(char tab[], unsigned int length){
     int i;
-    unsigned int retval=0;
-
+    unsigned int r=0;
     for(i=0; i < length; ++i){
-        retval += tab[i];
+        r += tab[i];
     }
-    return retval;
+    return r;
 }
 
 unsigned int sort(char window[], unsigned int length) {
-    unsigned int retval = 0;
+    unsigned int r = 0;
     int tmp = 0;
 	int i, j;
     for(i = 1; i < length; ++i){
-        for(j = 0; j < length-i; ++j){
+        for(j = 0; j < length - i; ++j){
             if(window[j] > window[j+1]){
                 tmp = window[j];
                 window[j] = window[j+1];
                 window[j+1] = tmp;
-                retval++;
+                r++;
             }
         }
     }
     qsort(window, length, sizeof(char), compare);
-    return retval;
+    return r;
 }
 
 void median_filter(IplImage *img_Filtered, IplImage *img_Original){
@@ -70,19 +69,21 @@ void median_filter(IplImage *img_Filtered, IplImage *img_Original){
     Data_original = (uchar*)img_Original -> imageData;
     Data_filtered = (uchar*)img_Filtered -> imageData;
     step_gray = img_Filtered -> widthStep/sizeof(uchar);
-    for(i = 1; i < height-1; ++i){
-        for(j = 1; j < width-1; ++j){
-            window[0] = Data_original[i*step_gray+j];
-            window[1] = Data_original[i*step_gray+(j-1)];
-            window[2] = Data_original[i*step_gray+(j+1)];
-            window[3] = Data_original[(i+1)*step_gray+(j)];
-            window[4] = Data_original[(i+1)*step_gray+(j-1)];
-            window[5] = Data_original[(i+1)*step_gray+(j+1)];
-            window[6] = Data_original[(i-1)*step_gray+(j)];
-            window[7] = Data_original[(i-1)*step_gray+(j-1)];
-            window[8] = Data_original[(i-1)*step_gray+(j+1)];
+    for(i = 1; i < height - 1; ++i){
+        for(j = 1; j < width - 1; ++j){
+            window[0] = Data_original[i*step_gray + j];
+            window[1] = Data_original[i*step_gray + (j-1)];
+            window[2] = Data_original[i*step_gray + (j+1)];
+
+            window[3] = Data_original[(i+1)*step_gray + (j)];
+            window[4] = Data_original[(i+1)*step_gray + (j-1)];
+            window[5] = Data_original[(i+1)*step_gray + (j+1)];
+
+            window[6] = Data_original[(i-1)*step_gray + (j)];
+            window[7] = Data_original[(i-1)*step_gray + (j-1)];
+            window[8] = Data_original[(i-1)*step_gray + (j+1)];
             sort(window, WINDOW_LEN);
-            Data_filtered[i*step_gray+j] = window[4];
+            Data_filtered[i*step_gray + j] = window[4];
         }
     }
 }
@@ -91,32 +92,31 @@ void filter_sobelX(IplImage *img_Filtered, IplImage *img_Original){
     int i, j, width, height, step_gray;
     uchar *Data_original, *Data_filtered;
     char window[WINDOW_LEN];
-    char tmp_imgData;
+    char tmp_img;
     width = MIN(img_Filtered -> width, img_Original -> width);
     height = MIN(img_Filtered -> height, img_Original -> height);
     Data_original = (uchar*)img_Original -> imageData;
     Data_filtered = (uchar*)img_Filtered -> imageData;
     step_gray = img_Filtered -> widthStep/sizeof(uchar);
-    for(i=1;i<height-1;++i){
-        for(j=1;j<width-1;++j){
-            window[0] = (char)Data_original[i*step_gray + j]*0;
-            window[1] = (char)Data_original[i*step_gray + (j-1)]*-1;
-            window[2] = (char)Data_original[i*step_gray + (j+1)]*1;
-            window[3] = (char)Data_original[(i+1)*step_gray + (j)]*0;
-            window[4] = (char)Data_original[(i+1)*step_gray + (j-1)]*-2;
-            window[5] = (char)Data_original[(i+1)*step_gray + (j+1)]*2;
-            window[6] = (char)Data_original[(i-1)*step_gray + (j)]*0;
-            window[7] = (char)Data_original[(i-1)*step_gray + (j-1)]*-1;
-            window[8] = (char)Data_original[(i-1)*step_gray + (j+1)]*1;
-            tmp_imgData = sum(window, WINDOW_LEN);
-            if(tmp_imgData > 255){
-                Data_filtered[i*step_gray + j] = 255;
-            }
-			else if(tmp_imgData < 0){
-                Data_filtered[i*step_gray + j] = 0;
+    for(i = 1; i < height - 1; ++i){
+        for(j = 1; j < width - 1; ++j){
+            window[0] = (char)Data_original[i * step_gray + j] * 0;
+            window[1] = (char)Data_original[i * step_gray + (j - 1)] * -1;
+            window[2] = (char)Data_original[i * step_gray + (j + 1)];
+
+            window[3] = (char)Data_original[(i + 1) * step_gray + (j)] * 0;
+            window[4] = (char)Data_original[(i + 1) * step_gray + (j - 1)] * -2;
+            window[5] = (char)Data_original[(i + 1) * step_gray + (j + 1)] * 2;
+
+            window[6] = (char)Data_original[(i - 1) * step_gray + j] * 0;
+            window[7] = (char)Data_original[(i - 1) * step_gray + (j - 1)] * -1;
+            window[8] = (char)Data_original[(i - 1) * step_gray + (j + 1)];
+            tmp_img = sum(window, WINDOW_LEN);
+            if(tmp_img < 0){
+                Data_filtered[i * step_gray + j] = 0;
             }
 			else{
-                Data_filtered[i*step_gray + j] = tmp_imgData;
+                Data_filtered[i * step_gray + j] = tmp_img;
             }
         }
     }
@@ -126,7 +126,7 @@ void filter_sobelY(IplImage *img_Filtered, IplImage *img_Original){
     int i, j, width, height, step_gray;
     uchar *Data_original, *Data_filtered;
     char window[WINDOW_LEN];
-    char tmp_imgData;
+    char tmp_img;
     width=MIN(img_Filtered->width, img_Original->width);
     height=MIN(img_Filtered->height, img_Original->height);
     Data_original=(uchar *)img_Original->imageData;
@@ -134,23 +134,21 @@ void filter_sobelY(IplImage *img_Filtered, IplImage *img_Original){
     step_gray=img_Filtered->widthStep/sizeof(uchar);
     for(i=1;i<height-1;++i){
         for(j=1;j<width-1;++j){
-            window[0] = (char)Data_original[i*step_gray+j]*-2;
-            window[1] = (char)Data_original[i*step_gray+(j-1)]*-1;
-            window[2] = (char)Data_original[i*step_gray+(j+1)]*-1;
-            window[3] = (char)Data_original[(i+1)*step_gray+(j)]*0;
-            window[4] = (char)Data_original[(i+1)*step_gray+(j-1)]*0;
-            window[5] = (char)Data_original[(i+1)*step_gray+(j+1)]*0;
-            window[6] = (char)Data_original[(i-1)*step_gray+(j)]*2;
-            window[7] = (char)Data_original[(i-1)*step_gray+(j-1)]*1;
-            window[8] = (char)Data_original[(i-1)*step_gray+(j+1)]*1;
-
-            tmp_imgData=sum(window, WINDOW_LEN);
-            if(tmp_imgData>255){
-                Data_filtered[i*step_gray+j]=255;
-            }else if(tmp_imgData<0){
-                Data_filtered[i*step_gray+j]=0;
-            }else{
-                Data_filtered[i*step_gray+j]=tmp_imgData;
+            window[0] = (char)Data_original[i * step_gray + j] * -2;
+            window[1] = (char)Data_original[i * step_gray + (j-1)] * -1;
+            window[2] = (char)Data_original[i * step_gray + (j+1)] * -1;
+            window[3] = (char)Data_original[(i+1) * step_gray + j] * 0;
+            window[4] = (char)Data_original[(i+1) * step_gray + (j-1)] * 0;
+            window[5] = (char)Data_original[(i+1) * step_gray + (j+1)] * 0;
+            window[6] = (char)Data_original[(i-1) * step_gray + j] * 2;
+            window[7] = (char)Data_original[(i-1) * step_gray + (j-1)];
+            window[8] = (char)Data_original[(i-1) * step_gray + (j+1)];
+            tmp_img = sum(window, WINDOW_LEN);
+            if(tmp_img < 0){
+                Data_filtered[i * step_gray + j] = 0;
+            }
+            else{
+                Data_filtered[i * step_gray + j] = tmp_img;
             }
         }
     }
@@ -167,21 +165,20 @@ int main(int argc, char const *argv[]){
 	IplImage *Image_IN;
 	IplImage *Image_OUT;
 	IplImage *Image_MED;
-	IplImage *Image_SOX; //image with sobel X filter
-	IplImage *Image_SOY; //image with sobel Y filter
-	// IplImage *Image_SOM; //image with both sobels merged
+	IplImage *Image_SOX;
+	IplImage *Image_SOY;
 
 	// Capture vidéo
     CvCapture *capture;
 
 	// variables images
-	int height,width,step,channels;		// Taille de l'image
-	uchar *Data_in;						// pointeur des données Image_IN
-	uchar *Data_out;					// pointeur des données Image_OUT
-	uchar *Data_Med;					// pointeur des données Image_MED
-	uchar *Data_Sox;					// pointeur des données Image_SOX
-	uchar *Data_Soy;					// pointeur des données Image_SOY
-	// uchar *Data_Som;					// pointeur des données Image_SOM
+	int height,width,step,channels;
+	uchar *Data_in;
+	uchar *Data_out;
+	uchar *Data_Med;
+	uchar *Data_Sox;
+	uchar *Data_Soy;
+
 
 	// int i,j,k;
 	int i, j;
@@ -202,14 +199,13 @@ int main(int argc, char const *argv[]){
 	cvNamedWindow("Image_MED_Window", CV_WINDOW_AUTOSIZE); 	// Image_MED
 	cvNamedWindow("Image_SOX_Window", CV_WINDOW_AUTOSIZE); 	// Image_SOX
 	cvNamedWindow("Image_SOY_Window", CV_WINDOW_AUTOSIZE); 	// Image_SOY
-	// cvNamedWindow("Image_SOM_Window", CV_WINDOW_AUTOSIZE); 	// Image_SOM
 
 	// Positionnement des fenêtres
-    cvMoveWindow("Image_IN_Window", 0,0);
-    cvMoveWindow("Image_OUT_Window", 0,700);
-	cvMoveWindow("Image_MED_Window", 700,0);
-	cvMoveWindow("Image_SOX_Window", 700,700);
-	cvMoveWindow("Image_SOY_Window", 1400,700);
+    cvMoveWindow("Image_IN_Window", 0, 0);
+    cvMoveWindow("Image_OUT_Window", 0, 500);
+	cvMoveWindow("Image_MED_Window", 500, 0);
+	cvMoveWindow("Image_SOX_Window", 500, 500);
+	cvMoveWindow("Image_SOY_Window", 1000, 500);
 	// cvMoveWindow("Image_SOM_Window", 1400,0);
 
 	// Première acquisition
@@ -219,17 +215,11 @@ int main(int argc, char const *argv[]){
 	Image_MED = cvCreateImage(cvSize(Image_IN -> width, Image_IN -> height),  IPL_DEPTH_8U, 1);
 	Image_SOX = cvCreateImage(cvSize(Image_IN->width,Image_IN->height),  IPL_DEPTH_8U, 1);
 	Image_SOY = cvCreateImage(cvSize(Image_IN->width,Image_IN->height),  IPL_DEPTH_8U, 1);
-	// Image_SOM = cvCreateImage(cvSize(Image_IN->width,Image_IN->height),  IPL_DEPTH_8U, 1);
 
 	int step_gray = Image_OUT -> widthStep/sizeof(uchar);
 
-    // Boucle tant que l'utilisateur n'appuie pas sur la touche q (ou Q)
-// #ifdef _PROFILING
-    // unsigned char _main_n_loops=MAIN_LOOPS;
-    // while(_main_n_loops--) {
-// #else
+
     while(ESC_keyboard != 'q' && ESC_keyboard != 'Q') {
-// #endif
 		// On récupère une Image_IN
 		Image_IN = cvQueryFrame(capture);
 		// Dimension
